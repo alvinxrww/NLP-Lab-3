9d3034659a3bf262c3b7b7454ca59bd5ab82735e
Alvin Xavier Rakha Wardhana <alvin.xavier@ui.ac.id>
2024-09-07 10:38:00 +0700

    init

---
+++ performance.py
@@ -0,0 +1,16 @@
+import time
+"""
+TODO: Gunakan file performance.py ini untuk membuat class Performance yang 
+nantinya akan digunakan untuk menghitung akurasi (Best Match Accuracy & Candidate Match) 
+dan run-time
+"""
+class Performance:
+    def __init__(self, model):
+        self.__model = model
+    
+    def calculate_performance(self):
+        return {
+            'cand_acc': -1, 
+            'best_acc': -1, 
+            'time': -1
+        }
---
+++ utils.py
@@ -0,0 +1,31 @@
+import json
+
+def load_json(json_path):
+    with open(json_path, 'r') as file:
+        json_dct = json.load(file)
+
+    return json_dct
+
+# diperbolehkan untuk mengubah fungsi ini sesuai kebutuhan
+def format_report(name, id, performances):
+    performance_report =  f"""
+{name} - {id}
+---------- lev_trie ----------
+best accuracy: {performances['lev_trie']['best_acc']}
+candidate accuracy: {performances['lev_trie']['cand_acc']}
+total_time: {performances['lev_trie']['time']} seconds
+---------- dalev_trie ----------
+best accuracy: {performances['dalev_trie']['best_acc']}
+candidate accuracy: {performances['dalev_trie']['cand_acc']}
+total_time: {performances['dalev_trie']['time']} seconds
+---------- lev_dict ----------
+best accuracy: {performances['lev_dict']['best_acc']}
+candidate accuracy: {performances['lev_dict']['cand_acc']}
+total_time: {performances['lev_dict']['time']} seconds
+---------- dalev_dict ----------
+best accuracy: {performances['dalev_dict']['best_acc']}
+candidate accuracy: {performances['dalev_dict']['cand_acc']}
+total_time: {performances['dalev_dict']['time']} seconds
+"""
+
+    return performance_report
